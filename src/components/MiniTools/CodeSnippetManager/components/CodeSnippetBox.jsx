381c55da36e0f272411d10c248117ec0e8402469
Tan Say Huan <sayhuan99@gmail.com>
2023-02-28 23:34:31 +0800

    fixed listing layout issue

--- src/components/MiniTools/CodeSnippetManager/components/CodeSnippetBox.jsx
+++ src/components/MiniTools/CodeSnippetManager/components/CodeSnippetBox.jsx
@@ -32,15 +32,15 @@ const CodeSnippetBox = ({ _id, name, purpose, type, language, codes, output, tag
   return (
     <Card className="border-2 border-indigo-300 !rounded-xl hover:outline-none hover:ring hover:ring-blue-200 hover:-translate-y-1 cursor-pointer">
       <CardContent className="!pb-2 !h-[300px]" onClick={handleViewCodeSnippet}>
-        <Box component="div" className="flex flex-row justify-between max-w-full">
+        <Box component="div" className="flex flex-row justify-between max-w-[300px]">
           <Typography sx={{ fontSize: 14 }} color="text-secondary" className="truncate overflow-ellipsis" gutterBottom>
             {name}
           </Typography>
           <Chip label={type.toUpperCase()} color={type === "code_snippet" ? "primary" : "success"} />
         </Box>
-        <Typography variant="h6">{language.toUpperCase()}</Typography>
+        <Typography variant="subtitle1"><strong>{language.toUpperCase()}</strong></Typography>
         <Typography className="text-gray-500 !mb-3 overflow-x-auto whitespace-nowrap p-1">{tags.map((t) => `#${t} `)}</Typography>
-        <Typography variant="body2" className="truncate overflow-ellipsis">
+        <Typography variant="body2" className="truncate overflow-ellipsis max-w-[300px]">
           {purpose}
         </Typography>
         <SyntaxHighlighter style={vscDarkPlus} language={language} className="h-[110px]">
